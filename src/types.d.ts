a7e6181e86845c58d67fa25653b9a01437a0ad9a
Kamyil <kkbugajak@gmail.com>
2022-01-25 19:40:55 +0100

    Corrected types delcarations for other IDE's

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,113 @@
 /**
+ * @description Makes given value reactive, which means that it will react and change on internal/other depdendent reactive value changes
+ * inside application and it will returns freshly updated value every single time
+ *
+ * @param value - Value that will react on changes or callback that will determine how initial and every freshly updated value will look like.
+ * If given reactive value is going to be independent - it can be passed directly.
+ * However if you want to make it dependent from other reactive values, you need to pass it as a callback in order
+ * to prevent JavaScript to early compute your value model with loosing reference to your variable in the same time.
+ * 
+ * @example
+ * ```ts
+ * // Like here - simple values can be passed directly
+ * let firstNumber = reactive(2);
+ * 
+ * // ... but if other values have to be dependent on other reactive values
+ * // then you have to pass it as a callback to prevent JavaScript from early computing your expression
+ * // into some value without any references to your variables
+ * let secondNumber = reactive(() => firstNumber.value * 2);
+ * console.log(secondNumber.value); // => 4
+
+ * firstNumber.value = 10;
+ *
+ * console.log(secondNumber.value); // => 20
+ * 
+ * // You can also pass arrays and objects
+ * let testArray = reactive([1, 2, 3]);
+ * let testObject = reactive({
+ *   name: 'Kamil',
+ *   age: 23
+ * });
+ * 
+ * // But they cannot be mutated since they will loose reference
+ * // Rather assign new object / array to them
+ * testArray.value = [...testArray.value, 4];
+ * testObject.value = {
+ *  ...testObject.value,
+ *  favoriteHero: 'Daredevil'
+ * } 
+ * 
+ * // If you use TypeScript, you can (but don't have to since generics do type inference here)
+ * // also pass your interfaces or types
+ * type Person = {
+ *   name: string;
+ *   age: number;
+ *   favoriteHero?: string;
+ * };
+ * 
+ * testObject = reactive<Person>({
+ *   name: 'Kamyil',
+ *   age: 24,
+ * });
+ * ```
+ * @returns Initial and updated on every change value
+ */
+declare function reactive<initialValueType>(
+  value: initialValueType | (() => initialValueType)
+): Reactive<initialValueType>;
+
+declare global {
+  interface Window {
+    $reactiveDataContainer: ReactiveDataContainer;
+  }
+  namespace NodeJS {
+    interface Global {
+      $reactiveDataContainer: ReactiveDataContainer;
+    }
+  }
+}
+
+/**
+ * Stops tracking given reactive value and optionally performs an given callback if any provided
+ * @param reactiveValue
+ * @param callbackOnTrackStop
+ */
+declare function stopTracking<reactiveValueType>(
+  reactiveValue: Reactive<reactiveValueType>,
+  callbackOnTrackStop?: ({ currentValue }: { currentValue: unknown }) => unknown
+);
+
+/**
+ * Allows to keep reactive value in sync with HTML element
+ * @param reactiveValue
+ * @param elementOrSelector
+ * @param options
+ */
+declare function syncWithHTML<reactiveValueType>(
+  reactiveValue: Reactive<reactiveValueType>,
+  elementOrSelector: HTMLElement | string,
+  options: {
+    useDangerousInnerHTML?: boolean;
+    callback?: ({
+      previousValue,
+      newValue,
+    }: ICallbackValues<reactiveValueType>) => unknown;
+  }
+);
+
+/**
+ * Allows to perform provided callbacks on given Reactive value change
+ * @param callback - Callback to perform
+ */
+declare function trackChanges<reactiveValueType>(
+  reactiveValueToTrack: Reactive<reactiveValueType>,
+  callback: ({
+    previousValue,
+    newValue,
+  }: ICallbackValues<reactiveValueType>) => void
+): void;
+
+/**
  * The definition of reactive data container that lives inside global (window or node.js global) context
  */
 export type ReactiveDataContainer = {
